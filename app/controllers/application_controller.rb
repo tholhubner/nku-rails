bfac4b852de6c111b535659df9a58beb495e74cd
Tyler Holhubner <tholhubner@gmail.com>
2014-03-26 20:15:49 +0000

    Fixed an issue with viewing assignments. forgot an s that caused an error when viewing the assignment page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,27 @@ class ApplicationController < ActionController::Base
     @current_student ||= Student.find(session[:student_id]) if session[:student_id].present?
   end
   helper_method :current_student
+  
+  def logged_in?
+    current_student != nil
+  end
+  
+  def authorize
+    unless current_student.admin_user?
+      flash[:error] = "Unauthorized user."
+      redirect_to root_path
+      false
+    end
+  end
+  
+  before_action :require_login
+  
+  skip_before_action :require_login, only: [:new, :create]
+  
+  private
+  def require_login
+    unless logged_in?
+      redirect_to signin_path
+    end
+  end
 end
--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,20 +1,35 @@
 class AssignmentsController < ApplicationController
   
+  ## checks if the student is an admin, if so allows viewing of new page
   def new
-    @assignment = Assignment.new
+    if current_student.admin_user?
+      @assignment = Assignment.new
+      @assignment.student_id = current_student.id
+    else
+      redirect_to root_path, notice: "You didn't say the magic word"
+    end
   end
   
+  ## show the assignments and their grades
+  # when an assignment is clicked, filter shown assignments by student_id
   def index
-    @assignment = Assignment.all
+    q = params[:search]
+    if current_student.admin_user?
+      current_student.name = "All Students"
+      if q.blank?
+        @assignments = Assignment.all
+      else
+        @assignments = Assignment.find(:all, :conditions => ["student_id LIKE ?", q])
+      end
+    else
+      @assignments = current_student.assignments
+    end
   end
   
+  #creation of a new assignment
   def create
     @assignment = Assignment.create!(assignment_params)
     redirect_to root_path, notice: "Assignment has been added"
-  end
-  
-  def view
-    
   end
   
   private 
